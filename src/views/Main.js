825261d99bd88e8674f830e3c1d480a97fdfb251
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-16 00:12:29 -0700

    loading feature is working!

--- src/views/Main.js
+++ src/views/Main.js
@@ -6,18 +6,23 @@ import fetchCountries from '../services/Countries';
 export default function Main() {
   const [countries, setCountries] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const resp = await fetchCountries();
         setCountries(resp);
+        setTimeout(() => {
+          setLoading(false);
+        }, 750);
       } catch (e) {
         setErrorMessage('Ooooopsie! Refresh page please!');
       }
     };
     fetchData();
   }, []);
+  if (loading) return <div>Loading</div>;
   return (
     <>
       <p className="error">{errorMessage}</p>
