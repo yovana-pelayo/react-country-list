4df2e2f9f408e7159697925637b7f12b5b0adee6
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-15 23:33:12 -0700

    names of countries are displaying. Found my syntax error

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import CountryCard from '../Components/CountryCard';
 import { client } from '../services/client';
 import fetchCountries from '../services/Countries';
 
 export default function Main() {
-  const [country, setCountry = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [countries, setCountries] = useState([]);
+  //   const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const resp = await client.fetchCountries();
-      console.log(resp);
+      const resp = await fetchCountries();
+      //   console.log(resp);
+      setCountries(resp);
     };
-  });
+    fetchData();
+  }, []);
+  return (
+    <div className="main">
+      {countries.map((country) => (
+        <CountryCard key={country.name} {...country} />
+      ))}
+    </div>
+  );
 }
